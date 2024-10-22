777934a064b7f19165191f828d4d7e7f7072ae2a
Gina Sprint <sprint@gonzaga.edu>
2024-10-21 20:07:42 -0700

    init

---
+++ mysklearn/mypytable.py
@@ -0,0 +1,3 @@
+from mysklearn import myutils
+
+# TODO: copy your mypytable.py solution from PA2-PA5 here
---
+++ test_myclassifiers.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from mysklearn.myclassifiers import MyNaiveBayesClassifier
+
+# TODO: copy your test_myclassifiers.py solution from PA4-5 here
+
+def test_naive_bayes_classifier_fit():
+    assert False is True # TODO: fix this
+
+def test_naive_bayes_classifier_predict():
+    assert False is True # TODO: fix this
